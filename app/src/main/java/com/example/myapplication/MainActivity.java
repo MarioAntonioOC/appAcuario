00e6c8d58f3236f4bbc553c0747ade79cb653190
Mario OC <oiram90140@gmail.com>
2022-07-21 15:59:57 -0500

    Se agrego la pantalla de login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,13 +25,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView logoImageView = findViewById(R.id.LogoImageView);
         logoImageView.setAnimation(animacion1);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        new Handler().postDelayed(()-> {
+
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
-            }
+
         }, 4000);
     }
 }
