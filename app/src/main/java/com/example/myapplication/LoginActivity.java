cd8f8fb52c83520a897f72e9b8ddc46916ea5768
Mario <oiram90140@gmail.com>
2022-08-12 14:13:29 -0500

    Logica areglada y cambio de tema

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,65 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView  bienvenidoLabel, continuarLabel, nuevoUsuario;
+    ImageView loginImageView;
+    TextInputLayout usuarioTexField, contrasenaTextField;
+    MaterialButton inicioSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginImageView = findViewById(R.id.signImageView);
+        bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
+        continuarLabel = findViewById(R.id.continuarLabel);
+        usuarioTexField = findViewById(R.id.usuarioSignUpTextField);
+        contrasenaTextField = findViewById(R.id.contrasenaTextField);
+        inicioSesion = findViewById(R.id.inicioSesion);
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+
+        nuevoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+
+                Pair[] pairs = new Pair[7];
+                pairs[0] = new Pair<View, String>(loginImageView, "logoImageTrans");
+                pairs[1] = new Pair<View, String>(bienvenidoLabel, "textTrans");
+                pairs[2] = new Pair<View, String>(continuarLabel, "iniciaSesionTextTrans");
+                pairs[3] = new Pair<View, String>(usuarioTexField, "emailInputTextTrans");
+                pairs[4] = new Pair<View, String>(contrasenaTextField, "passwordInputTextTrans");
+                pairs[5] = new Pair<View, String>(inicioSesion, "buttonSignInTrans");
+                pairs[6] = new Pair<View, String>(nuevoUsuario, "newUserTrans");
+                for(int i = 0; i < pairs.length; i++)
+                {
+                    // Imprimimos los elementos del array en pantalla.
+                    System.out.println(""+pairs[i]);
+                }
+
+               /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }else{ */
+                    startActivity(intent);
+                    finish();
+             //   }
+            }
+        });
     }
 }
